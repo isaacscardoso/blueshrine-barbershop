ac28c9b0d3a035b32306df1887b49510e486c98b
isaacscardoso <isaacscardosoblues@gmail.com>
2023-12-03 18:25:18 -0300

    feat: Implements the logo entry animation on the splash page.

--- lib/src/features/splash/splash_page.dart
+++ lib/src/features/splash/splash_page.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 
-final class SplashPage extends StatelessWidget {
+final class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  double _scale = 10.0;
+  double _logoOpacityAnimation = 0.0;
+
+  // Os valores sao diretamente proporcionais ao tamanho final da imagem.
+  double get _logoAnimationScaleWidth => 100.0 * _scale;
+  double get _logoAnimationScaleHeight => 120.0 * _scale;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _logoOpacityAnimation = 1.0;
+        _scale = 1.0;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +39,21 @@ final class SplashPage extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Image.asset('assets/images/imgLogo.png'),
+          child: AnimatedOpacity(
+            duration: const Duration(seconds: 2),
+            curve: Curves.easeIn,
+            opacity: _logoOpacityAnimation,
+            child: AnimatedContainer(
+              duration: const Duration(seconds: 4),
+              curve: Curves.linearToEaseOut,
+              width: _logoAnimationScaleWidth,
+              height: _logoAnimationScaleHeight,
+              child: Image.asset(
+                'assets/images/imgLogo.png',
+                scale: 0.8,
+              ),
+            ),
+          ),
         ),
       ),
     );
