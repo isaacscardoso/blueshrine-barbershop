239ccbf7eb9ed1d65cd110b90bc39e0346fa115f
isaacscardoso <isaacscardosoblues@gmail.com>
2023-12-09 22:43:51 -0300

    feat: Implements the initial design of the login page.

--- lib/src/features/auth/login/login_page.dart
+++ lib/src/features/auth/login/login_page.dart
@@ -6,10 +6,53 @@ final class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
+      backgroundColor: Colors.black,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            opacity: 0.3,
+            image: AssetImage('assets/images/background_image_chair.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: CustomScrollView(
+            slivers: [
+              SliverFillRemaining(
+                hasScrollBody: false,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Image.asset('assets/images/imgLogo.png'),
+                        TextFormField(
+                          decoration: const InputDecoration(
+                            label: Text('E-mail'),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Text(
+                        'Criar conta',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
