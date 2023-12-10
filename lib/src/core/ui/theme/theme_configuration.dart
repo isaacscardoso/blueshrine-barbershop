547c6211999c386e56693f86ca0387777e10f497
isaacscardoso <isaacscardosoblues@gmail.com>
2023-12-10 00:33:03 -0300

    feat: Implements the application's theme configuration file.

---
+++ lib/src/core/ui/theme/theme_configuration.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../styles/app_colors.dart';
+
+sealed class ThemeConfiguration {
+  static const _defaultInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(8),
+    ),
+    borderSide: BorderSide(color: AppColors.grey),
+  );
+
+  static ThemeData themeData = ThemeData(
+    useMaterial3: true,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: Colors.white,
+      labelStyle: const TextStyle(color: AppColors.grey),
+      border: _defaultInputBorder,
+      enabledBorder: _defaultInputBorder,
+      focusedBorder: _defaultInputBorder,
+      errorBorder: _defaultInputBorder.copyWith(
+        borderSide: const BorderSide(color: AppColors.red),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.brown,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+      ),
+    ),
+  );
+}
