70a0ba917f1ca505eb2a14094f7dad7c47c095d4
isaacscardoso <isaacscardosoblues@gmail.com>
2023-12-25 17:22:21 -0300

    feat: It implements dependency injection of the user's repository.

--- lib/src/core/providers/app_providers.dart
+++ lib/src/core/providers/app_providers.dart
@@ -1,8 +1,14 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import '../../repositories/user/user_repository.dart';
+import '../../repositories/user/user_repository_impl.dart';
 import '../rest_client/rest_client.dart';
 
 part 'app_providers.g.dart';
 
 @Riverpod(keepAlive: true)
 RestClient restClient(RestClientRef ref) => RestClient();
+
+@Riverpod(keepAlive: true)
+UserRepository userRepository(UserRepositoryRef ref) =>
+    UserRepositoryImpl(restClient: ref.read(restClientProvider));
